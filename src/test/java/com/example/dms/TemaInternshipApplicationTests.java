de56b56699d30c051511bc34d4585bb6121f79b5
Maximiliard5 <matei.oproiu@email.com>
2026-05-09 11:31:45 +0300

    Initial commit

---
+++ src/test/java/com/example/dms/TemaInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemaInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
