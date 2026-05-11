e051115e8efc4bbfb0732854d46d80670d2e583a
Maximiliard5 <matei.oproiu@email.com>
2026-05-11 21:10:44 +0300

    feat: file storage exception

---
+++ src/main/java/com/example/dms/exception/FileStorageException.java
@@ -0,0 +1,8 @@
+package com.example.dms.exception;
+
+public class FileStorageException extends RuntimeException {
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+}
